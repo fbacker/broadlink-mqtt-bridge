350bee6ad2538d8f1b6ff120a16249c2e2583ed4
Fredrick Bäcker <fredrick.backer@gmail.com>
2019-12-05 21:15:22 +0100

    fix custom logs port

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-
+import { exec } from 'child_process';
 import logger from './src/logger';
 import Webserver from './src/web';
+import config from './src/config';
+
 
 const run = () => {
   logger.info('Starting Broadlink MQTT NodeJS Application');
   Webserver.startServer();
+  exec(`frontail --port ${config.settings.gui.logs} --disable-usage-stats output.log`, () => {
+    // output is running
+  });
 };
 run();
