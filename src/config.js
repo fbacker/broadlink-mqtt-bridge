a98d16b270cc207fb60a3314c2517e2c00a17fba
Fredrick Bäcker <fredrick.backer@gmail.com>
2020-02-09 18:43:59 +0100

    remove ints convert, handle in openhab

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ import path from 'path';
 class Config {
   constructor(settings) {
     this.settings = settings;
-    this.settings.version = '2.2.0';
+    this.settings.version = '2.2.1';
     this.commandsPath = settings.recording.path || path.join(__dirname, '../', 'commands');
     this.isRunningRecording = false;
     this.isRunningScan = false;
