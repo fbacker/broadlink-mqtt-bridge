eabaa3551aa2ea0f93caa3b66592c9ea22b35b44
Fredrick Bäcker <fredrick.backer@gmail.com>
2020-11-12 21:55:05 +0100

    travis update

--- .travis-build.sh
+++ .travis-build.sh
@@ -22,7 +22,7 @@ buildctl build --frontend dockerfile.v0 \
       --local dockerfile=. \
       --local context=. \
       --exporter image \
-      --exporter-opt name=docker.io/$container:test-build \
+      --exporter-opt name=docker.io/$container:dev \
       --exporter-opt push=true \
       --frontend-opt platform=$platforms \
       --frontend-opt filename=./Dockerfile
@@ -35,13 +35,23 @@ do
       --local dockerfile=. \
       --local context=. \
       --exporter image \
-      --exporter-opt name=docker.io/$container:test-build-$arch \
+      --exporter-opt name=docker.io/$container:dev-$arch \
       --exporter-opt push=true \
       --frontend-opt platform=linux/$arch \
       --frontend-opt filename=./Dockerfile &
 done
 
 wait
+
+for arch in $architectures
+do
+    docker manifest annotate $container:dev $container:dev-$arch --arch $arch    
+done
+docker tag $container:dev-arm $container:dev-armhf
+docker push $container:dev-armhf
+docker manifest annotate $container:dev $container:dev-armhf --arch armhf
+
+docker manifest push $container:dev
 
 #docker pull $container:test-build-arm
 #docker tag $container:test-build-arm $container:test-build-armhf
